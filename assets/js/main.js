75669583dc3f27a9607a21341ac10017c8627200
AymenBida <aymen.bida.00@gmail.com>
2021-03-22 15:27:26 +0100

    Create a for loop to show books in a table

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-let myLibrary = [];
+const myLibrary = [];
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -11,3 +11,21 @@ function addBookToLibrary(title, author, pages, read) {
   myLibrary.push(new Book(title, author, pages, read));
 }
 
+addBookToLibrary('Alchemist', 'Paolo Coelho', 125, 'read');
+addBookToLibrary('Romeo and juliet', 'unknown', 360, 'not read');
+addBookToLibrary('Inception', 'Hans Zimmer', 450, 'read');
+addBookToLibrary('Tarzan', 'Disney', 122, 'not read');
+
+const myTable = document.querySelector('.myTable');
+
+for (let i = 0; i < myLibrary.length; i += 1) {
+  const tr = document.createElement('tr');
+  const myKeys = Object.keys(myLibrary[i]);
+  const myValues = Object.values(myLibrary[i]);
+  for (let j = 0; j < myKeys.length; j += 1) {
+    const th = document.createElement('th');
+    th.textContent = myValues[j];
+    tr.appendChild(th);
+  }
+  myTable.appendChild(tr);
+}
